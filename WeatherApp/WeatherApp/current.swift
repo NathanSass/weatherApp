9f1d73e5f2174bc902aff924e067e44864c59223
Nathan Sass <Nathan.Sass@chase.com>
2015-01-27 14:19:39 -0800

    added current weather struct

---
+++ WeatherApp/WeatherApp/current.swift
@@ -0,0 +1,23 @@
+//
+//  current.swift
+//  WeatherApp
+//
+//  Created by Sass, Nathan on 1/27/15.
+//  Copyright (c) 2015 Nathan Sass. All rights reserved.
+//
+
+import Foundation
+
+struct current {
+    var currentTime: Int?
+    var temperature: Int?
+    var humidity: Double?
+    var precipProbability: Double?
+    var summary: String?
+    var icon: String?
+    
+    init(){
+        
+        
+    }
+}
